756fc944ad02f22b393f1a70c6656b754d2955d7
Benjie <Balagso@gmail.com>
2024-09-10 20:16:06 +0800

    Add Modulus method

--- Calculator.java
+++ Calculator.java
@@ -6,11 +6,13 @@ public class calculator {
         int sub = a - b;
         int mul = a * b;
         int quo = a / b;
+        int mod = a % b;
 
         System.out.println("Addition " + sum);
         System.out.println("Subtraction " + sub);
         System.out.println("Multiplication " + mul);
         System.out.println("Division " + quo );
+        System.out.println("Modulus " + mod );
         
 
 
