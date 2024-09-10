0422a93756cd85b340bea6918a1d8d1a67d86dad
Benjie <Balagso@gmail.com>
2024-09-10 20:03:32 +0800

    Add Addition method

--- Calculator.java
+++ Calculator.java
@@ -2,6 +2,9 @@ public class calculator {
     public static void main (String[] args) {
         int a = 8;
         int b = 23;
+        int sum = a + b;
+
+        System.out.println("Addition " = sum);
 
 
         
